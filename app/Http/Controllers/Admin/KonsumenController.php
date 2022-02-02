11a2cd75b57b751cda667885b7008b06cfe737fc
IlyasArrafii <zainularrafii@gmail.com>
2022-02-02 11:20:29 +0700

    tambah controller

---
+++ app/Http/Controllers/Admin/KonsumenController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Konsumen;
+use Illuminate\Http\Request;
+
+class KonsumenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Konsumen  $konsumen
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Konsumen $konsumen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Konsumen  $konsumen
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Konsumen $konsumen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Konsumen  $konsumen
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Konsumen $konsumen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Konsumen  $konsumen
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Konsumen $konsumen)
+    {
+        //
+    }
+}
