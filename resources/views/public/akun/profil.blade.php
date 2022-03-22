f7696657dba7fd95e74c576abba17218b4149486
IlyasArrafii <zainularrafii@gmail.cm>
2022-03-22 09:02:17 +0700

    belom fix profile

--- resources/views/public/akun/profil.blade.php
+++ resources/views/public/akun/profil.blade.php
@@ -12,25 +12,40 @@
     <div class="tw-mx-3">
         <div class="tw-border-[#fff] tw-rounded-xl tw-border tw-mt-1 tw-shadow-md">
             <img src="assets/img/icon1/profile.png" class="tw-mx-auto tw-pt-2 tw-mb-1" style="width: 90px;">
-            <div class="tw-mx-auto tw-flex">
-                <img src="assets/img/icon1/profile.png" class="tw-mx-auto tw-pt-2 tw-mb-1 tw-flex-1" style="width: 44px;">
-                <p class="tw-my-1 tw-px-3 tw-font-poppins tw-text-sm tw-mt-3 tw-flex-1">Afif Fathurrahman</p>
+            <div class="tw-mx-auto ">
+                <div class="tw-flex tw-flex-row">
+                    <img src="assets/img/icon1/profile.png" class="tw-ml-3 tw-p-2" style="width: 35px;">
+                    <span class="tw-font-poppins tw-text-sm tw-text-center tw-mt-2 tw-ml-1">Afif Fathurrahman</span>
+                </div>
             </div>
-            <i class="fa-solid fa-phone tw-border-slate-600 tw-bg-orange-500"></i>
-            <p class="tw-my-1 tw-px-3 tw-font-poppins tw-text-sm tw-mt-1">Jl. H. Syukur VIC Blok C-14 Sedati, Sidoarjo </p>
-            <div class="tw-flex tw-pb-2">
-                <div class="tw-mx-auto tw-text-left tw-flex-1 tw-pl-3">
-                    <div class="tw-text tw-font-poppins tw-mb-3">
+            <div class="tw-mx-auto ">
+                <div class="tw-flex tw-flex-row">
+                    <img src="assets/img/icon1/telephone.png" class="tw-ml-3 tw-p-2" style="width: 35px;">
+                    <span class="tw-font-poppins tw-text-sm tw-text-center tw-mt-2 tw-ml-1">0895-34200-1126</span>
+                </div>
+            </div>
+            <div class="tw-mx-auto ">
+                <div class="tw-flex tw-flex-row">
+                    <img src="assets/img/icon1/at.png" class="tw-ml-3 tw-p-2" style="width: 35px;">
+                    <span class="tw-font-poppins tw-text-sm tw-text-center tw-mt-2 tw-ml-1">afif.fathurrahman03@gmail.com</span>
+                </div>
+            </div>
+            <div class="tw-flex tw-mt-3">
+                <div class="tw-mx-auto tw-flex-1">
+                    <div class="tw-text-sm tw-font-poppins tw-mr-[10%] tw-border tw-ml-5 tw-border-gray-600 tw-bg-white tw-my-1 tw-rounded-md">
                         <a href="{{url('/')}}" class="tw-no-underline" style="text-decoration: none;"><i class="fa-solid fa-arrow-left"></i><span class="tw-text-sm tw-p-1 tw-text-red-500">
-                                <i class="fa fa-phone"></i>Kembali
+                                Edit Profil
                             </span>
                         </a>
                     </div>
-
                 </div>
-                <div class="tw-text-center tw-mx-auto tw-mr-[40%] md:tw-mx-auto md:tw-mr-[46%] md:tw-text-center">
-                    <div class="tw-mx-auto">
-                        <span class="tw-m-auto tw-text-center tw-text-red-500 tw-font-poppins">Pesanan</span>
+                <div class="tw-text-center tw-mx-auto tw-mr-[10%] md:tw-mx-auto md:tw-mr-[46%] md:tw-text-center">
+                    <div class="tw-mx-auto tw-border tw-border-gray-600 tw-bg-white tw-my-1 tw-rounded-md tw-px-5">
+                        <a href="{{url('/')}}" class="tw-no-underline" style="text-decoration: none;"><i class="fa-solid fa-arrow-left"></i><span class="tw-text-sm tw-p-1 tw-text-red-500">
+                                Keluar Akun
+                            </span>
+                        </a>
+                        <!-- <span class="tw-m-auto tw-text-center tw-text-sm tw-text-red-500 tw-font-poppins">Pesanan</span> -->
                     </div>
                 </div>
             </div>
